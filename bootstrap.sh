e7ce3573972732313e3c2a7300d5e09538f30298
Jude <1559819+judewang@users.noreply.github.com>
2022-01-19 15:35:07 +0900

    fix: add nvm default-packages config

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,4 @@
 echo "Add default packages to nvm config"
+touch $HOME/.nvm/default-packages
 echo -e "@sanity/cli\nvercel\ncommitizen\ncz-conventional-changelog" >> $HOME/.nvm/default-packages
 echo "All done now!"
