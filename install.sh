0f9345421140b98f755d2fd61aac9abe2efd3435
Jude <jude@gapp.nthu.edu.tw>
2022-10-10 08:44:56 +0000

    fix: 🐛 fix gitpod dotfiles copy

--- install.sh
+++ install.sh
@@ -14,14 +14,18 @@ for file in ~/.dotfiles/files/*; do
   cp $file $GITPOD_REPO_ROOT
 done
 
+cp ~/.dotfiles/files/.gitpod.Dockerfile $GITPOD_REPO_ROOT
+cp ~/.dotfiles/files/.xinitrc $GITPOD_REPO_ROOT
+cp ~/.dotfiles/files/.Xresources $GITPOD_REPO_ROOT
+
 echo "Check Gitpod config"
 
-FILE=${GITPOD_REPO_ROOT}/.gitpod
+FILE=${GITPOD_REPO_ROOT}/.gitpod.yml
 if [ -f "$FILE" ]; then
-    echo "$FILE exists. Skip copy .gitpod"
+    echo "$FILE exists. Skip copy .gitpod.yml"
 else 
-    echo "$FILE does not exist. Copying default .gitpod"
-    cp ~/.dotfiles/.gitpod.example ${GITPOD_REPO_ROOT}/.gitpod
+    echo "$FILE does not exist. Copying default .gitpod.yml"
+    cp ~/.dotfiles/.gitpod.example ${GITPOD_REPO_ROOT}/.gitpod.yml
 fi
 
 cd $GITPOD_REPO_ROOT
