7c049a46274ee2ba977f5101f820502b36a864de
Jude <jude@gapp.nthu.edu.tw>
2022-10-12 08:03:21 +0000

    fix: 🐛 fix files copy process

--- install.sh
+++ install.sh
@@ -4,20 +4,6 @@ echo "Add default packages to nvm config"
 touch $HOME/.nvm/default-packages
 echo -e "@sanity/cli\nvercel\ncommitizen\ncz-conventional-changelog" >> $HOME/.nvm/default-packages
 
-echo "Copy VNC resources"
-
-TMPDIR=$(mktemp -d)
-
-cd $TMPDIR
-
-for file in ~/.dotfiles/files/*; do
-  cp $file $GITPOD_REPO_ROOT
-done
-
-cp ~/.dotfiles/files/.gitpod.Dockerfile $GITPOD_REPO_ROOT
-cp ~/.dotfiles/files/.xinitrc $GITPOD_REPO_ROOT
-cp ~/.dotfiles/files/.Xresources $GITPOD_REPO_ROOT
-
 echo "Check Gitpod config"
 
 FILE=${GITPOD_REPO_ROOT}/.gitpod.yml
@@ -26,10 +12,10 @@ if [ -f "$FILE" ]; then
 else 
     echo "$FILE does not exist. Copying default .gitpod.yml"
     cp ~/.dotfiles/.gitpod.example ${GITPOD_REPO_ROOT}/.gitpod.yml
+    echo "Copy VNC resources"
+    cp ~/.dotfiles/files/* $GITPOD_REPO_ROOT
 fi
 
 cd $GITPOD_REPO_ROOT
-
-rm -rf $TMPDIR
 
 echo "All done now!"
